bac8da75f4440f47294f71505ab57316fbdb0c44
Restyled.io <commits@restyled.io>
2020-01-09 11:27:29 +0000

    Restyled by clang-format

--- src/main/java/org/web3j/mavenplugin/solidity/Constant.java
+++ src/main/java/org/web3j/mavenplugin/solidity/Constant.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 
 public interface Constant {
 
-    Pattern SOLC_VERSION_PATTERN = Pattern.compile("Version: (.*)", Pattern.MULTILINE);
-    Pattern SOLIDITY_VERSION_EXTRACT = Pattern.compile("(pragma solidity.*;)", Pattern.MULTILINE);
-
+  Pattern SOLC_VERSION_PATTERN =
+      Pattern.compile("Version: (.*)", Pattern.MULTILINE);
+  Pattern SOLIDITY_VERSION_EXTRACT =
+      Pattern.compile("(pragma solidity.*;)", Pattern.MULTILINE);
 }
